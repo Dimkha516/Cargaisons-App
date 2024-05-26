f6af33b7896337fc2c8c898c464835ac8310812d
Dimkha <dimkha516@gmail.com>
2024-05-26 13:50:48 +0000

    ajout cargaison sans map

--- src/home.ts
+++ src/home.ts
@@ -1,3 +1,4 @@
+//-----------------------------------------------------FONCTION POUR GÉNÉRER UN CODE ALEATOIRE
 function generateRandomString(): string {
   const randomDigits = Math.floor(Math.random() * 100);
   const randomLetters =
@@ -6,8 +7,45 @@ function generateRandomString(): string {
 
   return `${randomDigits}-${randomLetters}`;
 }
-
 const randomCode = generateRandomString();
+//************************************************************************************************/ 
+
+
+//--------------------------------------------FONCTION POUR VÉRIFIER SI UNE VALEUR EST DE TYPE DATE:
+function isDate(value: any): boolean {
+  return value instanceof Date && !isNaN(value.getTime());
+}
+//************************************************************************************************/
+
+
+//--------------------------------------------FONCTION POUR CONVERTIR UN INPUT DATE EN DATE:
+function convertToDate(dateString: string): Date | null {
+  const date = new Date(dateString);
+  return isNaN(date.getTime()) ? null : date;
+}
+//************************************************************************************************/
+
+
+
+//--------------------------------------FONCTION POUR COMPARER DEUX INPUT DATE DEJAS CONVERTIES EN DATE:
+function compareDates(dateString1: string, dateString2: string): number | null {
+  const date1 = convertToDate(dateString1);
+  const date2 = convertToDate(dateString2);
+
+  if (date1 === null || date2 === null) {
+      return null; // Une des dates est invalide
+  }
+
+  if (date1 < date2) {
+      return -1; // date1 est antérieure à date2
+  } else if (date1 > date2) {
+      return 1; // date1 est postérieure à date2
+  } else {
+      return 0; // Les deux dates sont égales
+  }
+}
+//************************************************************************************************/
+
 
 // POINTAGE DES VARIABLES DU FORMULAIRE:
 const addCargForm = document.querySelector(".addCargaisonForm");
@@ -15,9 +53,9 @@ const cargPoids = document.getElementById("poidsCarg") as HTMLInputElement;
 const cargType = document.querySelector(".cargaisonType") as HTMLSelectElement;
 const cargStartZone = document.getElementById("depart") as HTMLInputElement;
 const cargEndZone = document.getElementById("arrivee") as HTMLInputElement;
-const cargDistance = document.getElementById(
-  "cargDistance"
-) as HTMLInputElement;
+const cargDistance = document.getElementById("cargDistance") as HTMLInputElement; //
+const cargStartDate = document.getElementById("dateDepart") as HTMLInputElement;
+const cargEndDate = document.getElementById("dateArrivee") as HTMLInputElement;
 
 // POINTAGE DES MESSAGES D'ERREUR:
 // const cargError = document.querySelector(".cargError") as HTMLElement;
@@ -25,12 +63,18 @@ const poidsError = document.querySelector(".poidsError") as HTMLElement;
 const typeError = document.querySelector(".typeError") as HTMLElement;
 const startZoneError = document.querySelector(".startZoneError") as HTMLElement;
 const endZoneError = document.querySelector(".endZoneError") as HTMLElement;
+const startDateError = document.querySelector(".startDateError") as HTMLElement;
+const endDateError = document.querySelector(".endDateError") as HTMLElement;
+const dateError = document.querySelector(".cargDateError") as HTMLElement;
 
 // ETAT DES ERREURS:
 let validPoids: boolean,
   validType: boolean,
   validStartZone,
-  validEndZone: boolean;
+  validEndZone: boolean,
+  validStartDate:boolean,
+  validEndDate:boolean,
+  validDate:boolean;
 
 //-------------------------------------------------AFFICHAGE CARD CARGAISONS AERIENNES
 async function showAirCard() {
@@ -179,8 +223,41 @@ addCargForm?.addEventListener("submit", (e) => {
     validEndZone = true;
   }
 
-  if (validPoids && validType && validStartZone && validEndZone) {
+  if(cargStartDate.value === ""){
+    startDateError.style.display = "block";
+    startDateError.innerHTML = "Date de départ non définie";
+    validStartDate = false;
+  } else{
+    startDateError.style.display = "none";
+    startDateError.innerHTML = "";
+    validStartDate = true;
+  }
+
+  if(cargEndDate.value === ""){
+    endDateError.style.display = "block";
+    endDateError.innerHTML = "Date de d'arrivée non définie";
+    validEndDate = false;
+  } else{
+    endDateError.style.display = "none";
+    endDateError.innerHTML = "";
+    validEndDate = true;
+  }
+
+  if(compareDates(cargStartDate.value, cargEndDate.value)===1){
+    dateError.style.display = "block";
+    dateError.innerHTML = "Date départ inférieure à date d'arrivée";
+    validDate = false;
+  }
+  else if(compareDates(cargStartDate.value, cargEndDate.value)=== -1){
+    dateError.style.display = "none";
+    dateError.innerHTML = "";
+    validDate = true;
+  }
+
+
+  if (validPoids && validType && validStartZone && validEndZone && validStartDate && validEndDate && validDate) {
     alert("Validée");
+    
 
     const newCarg = {
       numero: randomCode,
@@ -190,12 +267,13 @@ addCargForm?.addEventListener("submit", (e) => {
       montant: 500000,
       lieu_depart: cargStartZone.value,
       destination: cargEndZone.value,
+      depart: cargStartDate.value,
+      arrivee: cargEndDate.value,
       progression: "En attente",
       status: "Ouvert",
     };
     try {
       const addedCargaison = newCargaison(newCarg);
-      // showAllCargs();
       console.log(addedCargaison);
     } catch (error) {
       console.log("Error", error);
@@ -209,6 +287,17 @@ async function fetchCargaisons(): Promise<any[]> {
   return await response.json();
 }
 
+function getStatusColor(status: string): string {
+  switch (status.toLowerCase()) {
+    case "ferme":
+      return "red";
+    case "ouvert":
+      return "green";
+    default:
+      return "yellow";
+  }
+}
+
 async function fetchData() {
   const response = await fetch("http://localhost:3000/cargaisons");
   return await response.json();
@@ -217,11 +306,11 @@ async function fetchData() {
 function filterData(data: any[], searchTerm: string) {
   const lowerCaseSearchTerm = searchTerm.toLocaleLowerCase();
 
-  return data.filter(item => {
-    const numero = item.numero?.toString().toLocaleLowerCase() || '';
-    const type = item.type?.toString().toLocaleLowerCase() || '';
-    const lieu_depart = item.lieu_depart?.toString().toLocaleLowerCase() || '';
-    const destination = item.destination?.toString().toLocaleLowerCase() || '';
+  return data.filter((item) => {
+    const numero = item.numero?.toString().toLocaleLowerCase() || "";
+    const type = item.type?.toString().toLocaleLowerCase() || "";
+    const lieu_depart = item.lieu_depart?.toString().toLocaleLowerCase() || "";
+    const destination = item.destination?.toString().toLocaleLowerCase() || "";
 
     return (
       numero.includes(lowerCaseSearchTerm) ||
@@ -233,64 +322,60 @@ function filterData(data: any[], searchTerm: string) {
 }
 
 function renderData(data: any[]) {
-  const allCargs = document.querySelector(".allCargs") as HTMLTableSectionElement;
-  allCargs.innerHTML = ''; // Clear existing rows
+  const allCargs = document.querySelector(
+    ".allCargs"
+  ) as HTMLTableSectionElement;
+  allCargs.innerHTML = ""; // Clear existing rows
 
-  data.forEach(item => {
+  data.forEach((item) => {
     const row = document.createElement("tr") as HTMLTableRowElement;
+    const statusColor = getStatusColor(item.status);
     row.innerHTML = `
-      <th>${item.numero}</th>
-      <td>${item.type}</td>
-      <td>${item.poids} Kg</td>
-      <td>${item.lieu_depart}</td>
-      <td>${item.destination}</td>
-      <td>${item.montant} Fr</td>
-      <td>${item.colis.length}</td>
-      <td>
+      <th class="text-base">${item.numero}</th>
+      <td class="text-base">${item.type}</td>
+      <td class="text-base">${item.poids} Kg</td>
+      <td class="text-base">${item.lieu_depart}</td>
+      <td class="text-base">${item.destination}</td>
+      <td class="text-base">${item.montant} Fr</td>
+      <td class="text-base">${item.colis.length}</td>
+      <td style="color: ${statusColor};">${item.status}</td>
+      <td class="text-base">${item.progression}</td>
+      <td class="text-base">
         <button class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Modifier</button>
-        <input type="checkbox" />
+        <input type="checkbox" /> 
       </td>
     `;
     allCargs.appendChild(row);
   });
 }
 
-async function showAllCargs(searchTerm: string = '') {
+async function showAllCargs(searchTerm: string = "") {
   const data = await fetchData();
   const filteredData = filterData(data, searchTerm);
   renderData(filteredData);
 }
 
-document.getElementById("default-search")?.addEventListener('input', (event) => {
-  const searchTerm = (event.target as HTMLInputElement).value;
-  showAllCargs(searchTerm);
-})
+document
+  .getElementById("default-search")
+  ?.addEventListener("input", (event) => {
+    const searchTerm = (event.target as HTMLInputElement).value;
+    showAllCargs(searchTerm);
+  });
 showAllCargs();
 
-// async function showAllCargs() {
-//   const response = await fetch("http://localhost:3000/cargaisons");
-//   const data = await response.json();
 
-//   const allCargs = document.querySelector(".allCargs") as HTMLTableRowElement;
-//   const parent = allCargs.parentNode as HTMLTableSectionElement;
 
-//   data.forEach((item: any) => {
-//     const row = document.createElement("tr") as HTMLTableRowElement;
-//       row.innerHTML = `
-//       <th>${item.numero}</th>
-//       <td>${item.type}</td>
-//       <td>${item.poids} Kg</td>
-//       <td>${item.lieu_depart}</td>
-//       <td>${item.destination}</td>
-//       <td>${item.montant} Fr</td>
-//       <td>${item.colis.length}</td>
-//       <div>
-//       <button class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Modifier</button>
-//       <input type="checkbox" />
-//       </div>
-//       `;
-//       parent.appendChild(row);
 
-//   });
-// }
+
+
+
+
+
+
+
+
+
+
+
+
 
