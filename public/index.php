8f8d83515ef51a5e97408a18680d74b19c2862c3
Dimkha <dimkha516@gmail.com>
2024-05-31 21:49:48 +0000

    ajout cargaison avec integration map

--- public/index.php
+++ public/index.php
@@ -1,10 +1,21 @@
 <?php
-// http://localhost:8089/Cargaisons/public/index.php
+// http://localhost:8089/Cargaisons-App/public/index.php
 
 $currentPage = isset($_GET["page"]) ? ($_GET["page"]) : 'home';
 include_once "../pages/" .$currentPage . ".php";
 
 
+//-----------MANQUANTS
+// MAP
 
 
-// CORRIGER LINKS DE LA NAVBAR
+//-------NOUVELLES FONCTIONALITÉS POUR LUNDI 03/05/2024😢
+// Mettre carg fermée en cours:
+
+//  Déclarer cargaison comme perdue => tous ces prod seront perdus.
+
+//  Déclarer carg comme arrivée => Message au destinataire.
+
+//  Déclarer produit d'une cargaison arrivée comme perdue.
+
+ 
